23023f8f222e4cef0700c7afa21d152ca5648120
sergey.zverev <sergey.zverev@realize.dev>
2026-09-14 14:41:10 +0300

    искходиник редактора

--- tools/compileEpfErf.cmd
+++ tools/compileEpfErf.cmd
@@ -1,5 +1,5 @@
 @chcp 65001
-@set RUNNER_IBCONNECTION=/F./.build/ibservice
+@set VRUNNER_IBCONNECTION=/F./.build/ibservice
 rem Компилим из исходников обработку/отчет
 rem В парметр передавать КАТАЛОГ с внешним отчетом / обработкой
-call vrunner compileepf --nocacheuse %1 .build
+call vrunner epf compile --out .build %1 
